ba6d59fef33506b6b4e57a8ad98cf4ecbbc7308a
Arkadiusz Neumann <arek22337@gmail.com>
2022-03-15 23:22:21 +0100

    Update verify_header.sh

--- .github/verify_header.sh
+++ .github/verify_header.sh
@@ -3,8 +3,8 @@
 year=$(date +"%Y")
 regex="^(Copyright\(c\) )([0-9]{4}-([0-9]{4}))( Arek testowanko)"
 echo $year
-
-if [[  $1 =~ $regex ]]
+file=cat $1
+if [[  $file =~ $regex ]]
 then
     echo ${BASH_REMATCH[3]}
     if [[ $year == ${BASH_REMATCH[3]} ]]
