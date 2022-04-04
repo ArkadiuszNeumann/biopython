0e811043b6867354a597ab9bce95c19af03e2491
Arkadiusz Neumann <arek22337@gmail.com>
2022-04-04 21:10:19 +0200

    Create service-worker.published.js

---
+++ Client/wwwroot/service-worker.published.js
@@ -0,0 +1,5 @@
+const cacheVersion = '1';
+const cacheNamePrefix = 'offline-cache-';
+const cacheName = `${cacheNamePrefix}${self.assetsManifest.version}${cacheVersion}`;
+const offlineAssetsInclude = [ /\.dll$/, /\.pdb$/, /\.wasm/, /\.html/, /\.js$/, /\.json$/, /\.css$/, /\.woff$/, /\.png$/, /\.jpe?g$/, /\.gif$/, /\.ico$/, /\.blat$/, /\.dat$/ ];
+const offlineAssetsExclude = [ /^service-worker\.js$/ ];
