6db6ee3520000a6b8cb46d2840a44021f55a5b35
ArkadiuszNeumann <ArkadiuszNeumann@users.noreply.github.com>
2022-04-04 19:26:02 +0000

    Commit from GitHub Actions (pullrequest)

--- Client/wwwroot/service-worker.published.js
+++ Client/wwwroot/service-worker.published.js
@@ -1,4 +1,4 @@
-const cacheVersion = '1';
+const cacheVersion = '2';
 const cacheNamePrefix = 'offline-cache-';
 const cacheName = `${cacheNamePrefix}${self.assetsManifest.version}${cacheVersion}`;
 const offlineAssetsInclude = [ /\.dll$/, /\.pdb$/, /\.wasm/, /\.html/, /\.js$/, /\.json$/, /\.css$/, /\.woff$/, /\.png$/, /\.jpe?g$/, /\.gif$/, /\.ico$/, /\.blat$/, /\.dat$/ ];
