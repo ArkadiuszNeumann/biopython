4efc438ff3fef9de600718c883ad7c451da2f042
Arkadiusz Neumann <arek22337@gmail.com>
2022-04-08 13:00:47 +0200

    Update DBUtils.py

--- BioSQL/DBUtils.py
+++ BioSQL/DBUtils.py
@@ -1,8 +1,8 @@
-# Copyright 2002 by Andrew Dalke.  All rights reserved.
+
 # Revisions 2007-2010 copyright by Peter Cock.  All rights reserved.
 # Revisions 2009 copyright by Brad Chapman.  All rights reserved.
 # Revisions 2013 copyright by Tiago Antao.  All rights reserved.
-#
+# Copyright(c) 2022 by Andrew Dalke.  All rights reserved.
 # This file is part of the Biopython distribution and governed by your
 # choice of the "Biopython License Agreement" or the "BSD 3-Clause License".
 # Please see the LICENSE file that should have been included as part of this
